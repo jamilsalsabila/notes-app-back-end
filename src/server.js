20f2983c84b3db3d6418e4e5c23e93776ef08a86
Salsabila Jamil <user@users-MacBook-Air.local>
2021-11-21 03:56:35 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi');
+<<<<<<< HEAD
 const routes = require('./routes');
 
 const init = async () => {
@@ -16,6 +17,33 @@ const init = async () => {
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
+=======
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+
+const init = async () => {
+	const notesService = new NotesService();
+
+	const server = Hapi.server({
+		port: 5000,
+		host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+		routes: {
+			cors: {
+				origin: ['*'],
+			},
+		},
+	});
+
+	await server.register({
+		plugin: notes,
+		options: {
+			service: notesService,
+		},
+	});
+
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
+>>>>>>> cb01377 (restructure with hapi plugin)
 };
 
 init();
