ef53e071042c1dc089d03a55e74a1dde0fe63b52
Salsabila Jamil <user@users-MacBook-Air.local>
2021-12-15 18:35:48 +0700

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,10 +3,34 @@ const bcrypt = require('bcrypt');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
 	constructor() {
 		this._pool = new Pool();
+	}
+
+	async verifyUserCredential(username, password) {
+		const query = {
+			text: 'SELECT id, password FROM users WHERE username = $1',
+			values: [username],
+		};
+
+		const result = await this._pool.query(query);
+
+		if (!result.rows.length) {
+			throw new AuthenticationError('Kredential yang Anda berikan salah');
+		}
+
+		const { id, password: hashedPassword } = result.rows[0];
+
+		const match = await bcrypt.compare(password, hashedPassword);
+
+		if (!match) {
+			throw new AuthenticationError('Kredensial yang Anda berikan salah');
+		}
+
+		return id;
 	}
 
 	async addUser({ username, password, fullname }) {
