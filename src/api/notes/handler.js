0745402b86bb381faa79aabe9ed8814e25461050
Salsabila Jamil <user@192.168.1.30>
2021-12-24 20:53:21 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -66,7 +66,7 @@ class NotesHandler {
 			const { id } = request.params;
 			const { id: credentialId } = request.auth.credentials;
 
-			await this._service.verifyNoteOwner(id, credentialId);
+			await this._service.verifyNoteAccess(id, credentialId);
 			const note = await this._service.getNoteById(id);
 
 			const response = h.response({
@@ -103,7 +103,7 @@ class NotesHandler {
 			const { id } = request.params;
 			const { id: credentialId } = request.auth.credentials;
 
-			await this._service.verifyNoteOwner(id, credentialId);
+			await this._service.verifyNoteAccess(id, credentialId);
 			const { title, body, tags } = request.payload;
 			await this._service.editNoteById(id, { title, body, tags });
 
@@ -136,7 +136,9 @@ class NotesHandler {
 	async deleteNoteByIdHandler(request, h) {
 		try {
 			const { id } = request.params;
+			const { id: credentialId } = request.auth.credentials;
 
+			await this._service.verifyNoteOwner(id, credentialId);
 			await this._service.deleteNoteById(id);
 
 			const response = h.response({
